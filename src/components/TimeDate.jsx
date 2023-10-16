1c86825f65cd23bc9aef48e44641970c7e1cb686
Kashif Khan <KashifKhnx04@gmail.com>
2023-10-16 18:22:44 +0500

    fixing time and date bug

--- src/components/TimeDate.jsx
+++ src/components/TimeDate.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { DateTime } from 'luxon';
 
 const TimeDate = (props) => {
 
-  function convertTimestamp(timestamp, timeZone) {
-    const dateTime = DateTime.fromSeconds(timestamp).setZone(timeZone);
-    const formattedTime = dateTime.toFormat('HH:mm');
-    return formattedTime;
+  const [currentTime, setCurrentTime] = useState(getCurrentTime());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(getCurrentTime());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  function getCurrentTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    return `${hours}:${minutes}`;
   }
+
+  function formateDate() {
+    const date = new Date();
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const dayOfWeek = daysOfWeek[date.getUTCDay()];
+    const day = date.getUTCDate();
+    const month = months[date.getUTCMonth()];
+    const formattedDate = `${dayOfWeek}, ${day} ${month}`;
+    return formattedDate;
+  }
+  
   return (
     <section className={props.darkMode ? 'time-container dark-mode' : 'time-container'}>
       <h2 className="location">{props.currentWeatherData.name} {props.currentWeatherData.sys.country}</h2>
-      <h2 className='time'>{convertTimestamp(props.currentWeatherData.dt, props.currentWeatherData.timezone)}</h2>
-      <p className='date'>{ }</p>
+      <h2 className='time'>{currentTime}</h2>
+      <p className='date'>{formateDate()}</p>
     </section>
   )
 }
